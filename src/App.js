d05f959871a0771f608d54a13a2a3bad053fcb7c
michellejt <mtongas@gmail.com>
2023-09-05 23:57:52 +0930

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import * as Yup from "yup";
+
+import { useFormik } from "formik";
 
 function App() {
+  const events = ["Running", "Walking", "Other"];
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      event: events[0],
+      age: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .label("Full Name")
+        .required()
+        .test(
+          "is-full-name",
+          "Please enter both your first and last name",
+          function (value) {
+            const nameArr = value.split(" ");
+            return nameArr.length >= 2;
+          }
+        ),
+      email: Yup.string().email().required(),
+      event: Yup.string().oneOf(events, "The event you chose does not exist"),
+      age: Yup.number()
+        .min(15, "You need to be older than 15 to register")
+        .required(),
+    }),
+    onSubmit: function (values) {
+      alert(`You are registered! Name: ${values.name}. Email: ${values.email}. event: ${values.event}. 
+        Age: ${values.age}`);
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 min-w-screen min-h-screen overflow-x-hidden">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="max-w-lg mx-auto bg-white rounded shadow-lg mt-7 p-3"
+      >
+        <h1 className="text-3xl mb-3 text-center">Sign up</h1>
+        <div className="mb-4">
+          <label for="name">Full Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className={`block w-full rounded border py-1 px-2 ${
+              formik.touched.name && formik.errors.name
+                ? "border-red-400"
+                : "border-gray-300"
+            }`}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+          />
+          {formik.touched.name && formik.errors.name && (
+            <span className="text-red-400">{formik.errors.name}</span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className={`block w-full rounded border py-1 px-2 ${
+              formik.touched.email && formik.errors.email
+                ? "border-red-400"
+                : "border-gray-300"
+            }`}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email && (
+            <span className="text-red-400">{formik.errors.email}</span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label for="event">event</label>
+          <select
+            name="event"
+            id="event"
+            className={`block w-full rounded border py-1 px-2 ${
+              formik.touched.event && formik.errors.event
+                ? "border-red-400"
+                : "border-gray-300"
+            }`}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.event}
+          >
+            {events.map((event, index) => (
+              <option value={event} key={index}>
+                {event}
+              </option>
+            ))}
+          </select>
+          {formik.touched.event && formik.errors.event && (
+            <span className="text-red-400">{formik.errors.event}</span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label for="age">Age</label>
+          <input
+            type="number"
+            name="age"
+            id="age"
+            className={`block w-full rounded border py-1 px-2 ${
+              formik.touched.age && formik.errors.age
+                ? "border-red-400"
+                : "border-gray-300"
+            }`}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.age}
+          />
+          {formik.touched.age && formik.errors.age && (
+            <span className="text-red-400">{formik.errors.age}</span>
+          )}
+        </div>
+        <div className="text-center">
+          <button className="bg-blue-500 rounded p-3 text-white" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
